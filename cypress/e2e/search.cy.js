cad7669209742f3bebb1848bd4ae5e23eeb17218
Wellsx <46058305+Wellsx@users.noreply.github.com>
2022-08-30 13:40:02 +0200

    Updated url

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -2,13 +2,14 @@ describe('Search function', () => {
   it('Search for an item using the search bar', () => {
     cy.visit('/');
     cy.get('#search_query_top')
+      .should('be.visible')
       .clear()
       .type('Dress')
       .type('{enter}')
       .should('have.value', 'Dress');
 
-    cy.url().should('contain', 'controller=search');
-    cy.get('.navigation_page').should('have.text', 'Search');
-    cy.get('.lighter').should('contain', 'Dress');
+    cy.url().should('eq', Cypress.config().baseUrl + 'controller=search');
+    cy.get('.navigation_page').should('be.visible').and('have.text', 'Search');
+    cy.get('.lighter').should('be.visible').and('contain', 'Dress');
   });
 });
